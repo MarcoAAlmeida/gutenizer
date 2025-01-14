dc2f252b10c9ca7e4e8d08ce9083d66fa36ead24
MarcoAAlmeida <marcoalmeida.dev.br@gmail.com>
2025-01-14 19:39:04 -0300

    added simple screenshot capture example

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <main>
+    <nuxt-link to="/capture">capture</nuxt-link>
     <ImageGallery />
     <RedirectsPanel />
     <MessagesPanel />
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,6 +15,7 @@ export default defineNuxtConfig({
     kv: true,
     blob: true,
     cache: true,
+    browser: true,
   },
   nitro: {
     experimental: {
